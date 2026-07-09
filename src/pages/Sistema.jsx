ee32aeed96d0fda6f2b530b81691df22f8630b2e
Gilson Faria do Carmo <nosligfc@gmail.com>
2026-07-09 17:43:43 -0300

    Melhora layout do menu principal

--- src/pages/Sistema.jsx
+++ src/pages/Sistema.jsx
@@ -5,70 +5,70 @@ import Clientes from "./Clientes";
 import Agenda from "./Agenda";
 import Financeiro from "./Financeiro";
 
+
 function Sistema() {
 
   const [pagina, setPagina] = useState("dashboard");
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        minHeight: "100vh"
-      }}
-    >
 
-      <aside
-        style={{
-          width: "220px",
-          background: "#111827",
-          color: "white",
-          padding: "20px"
-        }}
-      >
+  return (
+
+    <div className="layout">
+
+
+      <aside className="sidebar">
+
 
         <h2>NegócioAI</h2>
+
 
         <button onClick={() => setPagina("dashboard")}>
           🏠 Painel de controle
         </button>
 
-        <br /><br />
 
         <button onClick={() => setPagina("clientes")}>
           👥 Clientes
         </button>
 
-        <br /><br />
 
         <button onClick={() => setPagina("agenda")}>
           📅 Agenda
         </button>
-<br /><br />
 
-<button onClick={() => setPagina("financeiro")}>
-  💰 Financeiro
-</button>
+
+        <button onClick={() => setPagina("financeiro")}>
+          💰 Financeiro
+        </button>
+
+
       </aside>
 
 
-      <main
-        style={{
-          flex: 1,
-          padding: "30px"
-        }}
-      >
+
+      <main className="content">
+
 
         {pagina === "dashboard" && <Dashboard />}
 
+
         {pagina === "clientes" && <Clientes />}
+
 
         {pagina === "agenda" && <Agenda />}
 
-    
-{pagina === "financeiro" && <Financeiro />}
-</main>
+
+        {pagina === "financeiro" && <Financeiro />}
+
+
+      </main>
+
+
     </div>
+
   );
+
 }
+
 
 export default Sistema;
